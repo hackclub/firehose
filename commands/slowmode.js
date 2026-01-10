9d7ecb6697a0e3cebdb7bbef672b2319711729c1
Junya <junjia.liu@pm.me>
2026-01-10 22:15:54 +0000

    write prisma interaction for slowmode command

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -1,5 +1,6 @@
 const chrono = require('chrono-node');
 const { getPrisma } = require('../utils/prismaConnector');
+const getChannelManagers = require("../utils/isChannelManger");
 
 
 async function slowmode(args) {
@@ -9,27 +10,80 @@ async function slowmode(args) {
     const commands = text.split(" ");
     const userInfo = await client.users.info({ user: user_id });
     const isAdmin = userInfo.user.is_admin;
-    const channel = commands[0].split('|')[0].replace("<#", "");
-    let count = Number(commands[1]);
-    let time = Number(commands[2]);
+    const channelManagers = await getChannelManagers(channel_id);
+    const time = Number(commands[commands[0] && commands[0].includes('#') ? 1: 0])
 
-    if (!isAdmin) return;
+    let channel = channel_id;
+    if (commands[0] && commands[0].includes('#')) {
+        channel = commands[0].split('|')[0].replace("<#", "").replace(">", "");
+    }
 
-  
-   const createSlowMode = await prisma.Slowmode.create({
-      data: {
-      channel: channel,
-      locked: true,
-      time: time,
-      messageCount: count,
-      }
-    })
+    const errors = []
+    if (!isAdmin && !channelManagers.includes(user_id)) errors.push("Only admins can run this command.");
+    if (!channel) errors.push("You need to give a channel to make it read only");
+    if (!time) errors.push("You need to specify a valid time in seconds");
+
+    if (errors.length > 0)
+        return await client.chat.postEphemeral({
+            channel: `${channel_id}`,
+            user: `${user_id}`,
+            text: errors.join("\n")
+        });
+
+    const existingSlowmode = await prisma.Slowmode.findFirst({
+        where: { channel: channel }
+    });
+
+    try {
+        if (existingSlowmode) {
+            if (time === 0) {
+                await prisma.Slowmode.delete({
+                    where: {id: existingSlowmode.id},
+                });
+
+                await client.chat.postMessage({
+                    channel: process.env.MIRRORCHANNEL,
+                    text: `<@${user_id}> turned off slowmode in <#${channel}>`
+                });
+                await client.chat.postMessage({
+                    channel: channel,
+                    user: user_id,
+                    text: `Slowmode has been enabled - ${time.toString()} second wait between each message`
+                });
+            } else {
+                await prisma.Slowmode.update({
+                    where: {id: existingSlowmode.id},
+                    data: {
+                        locked: true,
+                        time: time,
+                    }
+                });
+            }
+        } else {
+            await prisma.Slowmode.create({
+                data: {
+                    channel: channel,
+                    locked: true,
+                    time: time,
+                }
+            });
+
+            await client.chat.postMessage({
+                channel: process.env.MIRRORCHANNEL,
+                text: `<@${user_id}> turned on slowmode in <#${channel}> - ${time.toString()} second wait between each message`
+            });
+            await client.chat.postMessage({
+                channel: channel,
+                user: user_id,
+                text: `Slowmode has been enabled - ${time.toString()} second wait between each message`
+            });
+        }
+    } catch(e) {
+        console.log(e);
+    }
 
     // TODO: send message in firehouse logs
     // TODO: cancel slowmode
-
- 
-    
-  }
+    }
 
 module.exports = slowmode;
