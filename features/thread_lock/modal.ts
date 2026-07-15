89b4856923661aaeeda7ea92367fc83d3cf0f83d
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-07-15 16:59:06 +0300

    Revert "Merge pull request #33 from 0xDracula/feat/locking-threads"
    This reverts commit 8e18675c14ac749155f751b4eda42ba181ad0003, reversing
    changes made to c6cfa1efd65c8630fca5ef510c768678f2068671.

--- features/thread_lock/modal.ts
+++ features/thread_lock/modal.ts
@@ -1,137 +1,129 @@
 import type { App } from '@slack/bolt';
 import {
-  getPrisma,
-  postMessage,
-  addReaction,
-  logInternal,
-  getThreadLink,
-  client,
-  env,
-  isUserAPIAvailable,
-  lockMessage,
-  lockThread,
+    getPrisma,
+    postMessage,
+    addReaction,
+    logInternal,
+    getThreadLink,
+    client,
+    env,
+    isUserAPIAvailable,
+    lockMessage,
 } from '../../utils/index.js';
 
 function registerModal(app: App) {
-  const prisma = getPrisma();
+    const prisma = getPrisma();
 
-  app.view('lock_modal', async ({ view, ack, body, respond }) => {
-    let { thread_id, channel_id } = JSON.parse(view.private_metadata) as {
-      thread_id: string;
-      channel_id: string;
-    };
+    app.view('lock_modal', async ({ view, ack, body, respond }) => {
+        let { thread_id, channel_id } = JSON.parse(view.private_metadata) as {
+            thread_id: string;
+            channel_id: string;
+        };
 
-    const submittedValues = view.state.values;
-    let reason: string | undefined;
-    let expires: Date | undefined;
+        const submittedValues = view.state.values;
+        let reason: string | undefined;
+        let expires: Date | undefined;
 
-    for (let key in submittedValues) {
-      if (submittedValues[key]['plain_text_input-action'])
-        reason = submittedValues[key]['plain_text_input-action'].value ?? undefined;
-      if (submittedValues[key]['datetimepicker-action']) {
-        const timestamp = submittedValues[key]['datetimepicker-action'].selected_date_time;
-        if (timestamp) {
-          expires = new Date(timestamp * 1000);
+        for (let key in submittedValues) {
+            if (submittedValues[key]['plain_text_input-action'])
+                reason = submittedValues[key]['plain_text_input-action'].value ?? undefined;
+            if (submittedValues[key]['datetimepicker-action']) {
+                const timestamp = submittedValues[key]['datetimepicker-action'].selected_date_time;
+                if (timestamp) {
+                    expires = new Date(timestamp * 1000);
+                }
+            }
         }
-      }
-    }
 
-    if (!reason) {
-      await ack({
-        response_action: 'errors',
-        errors: {
-          'plain_text_input-action': 'Please provide a reason.',
-        },
-      });
-      return;
-    }
-    if (!expires) {
-      await ack({
-        response_action: 'errors',
-        errors: {
-          'datetimepicker-action': 'Please provide an expiration time.',
-        },
-      });
-      return;
-    }
-    if (new Date() > expires) {
-      await ack({
-        response_action: 'errors',
-        errors: {
-          'datetimepicker-action': 'Time cannot be in the past.',
-        },
-      });
-      return;
-    }
+        if (!reason) {
+            await ack({
+                response_action: 'errors',
+                errors: {
+                    'plain_text_input-action': 'Please provide a reason.',
+                },
+            });
+            return;
+        }
+        if (!expires) {
+            await ack({
+                response_action: 'errors',
+                errors: {
+                    'datetimepicker-action': 'Please provide an expiration time.',
+                },
+            });
+            return;
+        }
+        if (new Date() > expires) {
+            await ack({
+                response_action: 'errors',
+                errors: {
+                    'datetimepicker-action': 'Time cannot be in the past.',
+                },
+            });
+            return;
+        }
 
-    ack();
+        ack();
 
-    await Promise.all([
-      prisma.log.create({
-        data: {
-          thread_id: thread_id,
-          admin: body.user.id,
-          lock_type: 'lock',
-          time: expires,
-          reason,
-          channel: channel_id,
-          active: true,
-        },
-      }),
+        await Promise.all([
+            prisma.log.create({
+                data: {
+                    thread_id: thread_id,
+                    admin: body.user.id,
+                    lock_type: 'lock',
+                    time: expires,
+                    reason,
+                    channel: channel_id,
+                    active: true,
+                },
+            }),
 
-      prisma.thread.upsert({
-        where: {
-          id: thread_id,
-        },
-        create: {
-          id: thread_id,
-          admin: body.user.id,
-          lock_type: 'lock',
-          time: expires,
-          reason,
-          channel: channel_id,
-          active: true,
-        },
-        update: {
-          admin: body.user.id,
-          lock_type: 'lock',
-          time: expires,
-          reason,
-          channel: channel_id,
-          active: true,
-        },
-      }),
-    ]);
+            prisma.thread.upsert({
+                where: {
+                    id: thread_id,
+                },
+                create: {
+                    id: thread_id,
+                    admin: body.user.id,
+                    lock_type: 'lock',
+                    time: expires,
+                    reason,
+                    channel: channel_id,
+                    active: true,
+                },
+                update: {
+                    admin: body.user.id,
+                    lock_type: 'lock',
+                    time: expires,
+                    reason,
+                    channel: channel_id,
+                    active: true,
+                },
+            }),
+        ]);
 
-    const logMessage = `<@${body.user.id}> locked a thread in <#${channel_id}> until ${expires.toLocaleString('en-US', { timeZone: 'America/New_York', timeStyle: 'short', dateStyle: 'long' })} EST for ${reason}.\nLink: ${getThreadLink(channel_id, thread_id)}`;
+        const logMessage = `<@${body.user.id}> locked a thread in <#${channel_id}> until ${expires.toLocaleString('en-US', { timeZone: 'America/New_York', timeStyle: 'short', dateStyle: 'long' })} EST for ${reason}.\nLink: ${getThreadLink(channel_id, thread_id)}`;
 
-    const [publicLogResult] = await Promise.all([
-      client.chat.postMessage({
-        channel: env.SLACK_LOG_CHANNEL,
-        text: logMessage,
-      }),
-      logInternal(logMessage),
-      postMessage(
-        channel_id,
-        `This thread is locked until ${expires.toLocaleString('en-US', { timeZone: 'America/New_York', timeStyle: 'short', dateStyle: 'long' })} EST for ${reason}.`,
-        thread_id
-      ),
-      addReaction(channel_id, 'lock', thread_id),
-    ]);
+        const [publicLogResult] = await Promise.all([
+            client.chat.postMessage({
+                channel: env.SLACK_LOG_CHANNEL,
+                text: logMessage,
+            }),
+            logInternal(logMessage),
+            postMessage(
+                channel_id,
+                `This thread is locked until ${expires.toLocaleString('en-US', { timeZone: 'America/New_York', timeStyle: 'short', dateStyle: 'long' })} EST for ${reason}.`,
+                thread_id
+            ),
+            addReaction(channel_id, 'lock', thread_id),
+        ]);
 
-    if (isUserAPIAvailable) {
-      try {
-        await lockThread(channel_id, thread_id);
-      } catch { }
-
-      if (publicLogResult?.ts) {
-        try {
-          await lockMessage(env.SLACK_LOG_CHANNEL, publicLogResult.ts);
-        } catch { }
-      }
-    }
-
-  });
+        if (isUserAPIAvailable && publicLogResult?.ts) {
+            try {
+                await lockMessage(env.SLACK_LOG_CHANNEL, publicLogResult.ts);
+            } catch {}
+        }
+    });
 }
 
 export default registerModal;
--- features/thread_lock/shortcut.ts
+++ features/thread_lock/shortcut.ts
@@ -1,228 +1,214 @@
 import type { App } from '@slack/bolt';
 
 import {
-  getPrisma,
-  isUserAdmin,
-  postEphemeral,
-  postMessage,
-  removeReaction,
-  logInternal,
-  getThreadLink,
-  env,
-  isUserAPIAvailable,
-  lockMessage,
-  lockThread,
-  unlockThread,
+    getPrisma,
+    isUserAdmin,
+    postEphemeral,
+    postMessage,
+    removeReaction,
+    logInternal,
+    getThreadLink,
+    env,
+    isUserAPIAvailable,
+    lockMessage,
 } from '../../utils/index.js';
 
 function registerShortcuts(app: App) {
-  const prisma = getPrisma();
+    const prisma = getPrisma();
 
-  app.shortcut('lock_thread', async ({ ack, body, client }) => {
-    ack();
+    app.shortcut('lock_thread', async ({ ack, body, client }) => {
+        ack();
 
-    if (body.type !== 'message_action') return;
-    const { channel, message, trigger_id, user } = body;
+        if (body.type !== 'message_action') return;
+        const { channel, message, trigger_id, user } = body;
 
-    const [, isAdmin] = await Promise.all([
-      client.conversations
-        .join({
-          channel: channel.id,
-        })
-        .catch(() => { }),
-      isUserAdmin(user.id),
-    ]);
-    const thread_ts = message.thread_ts || message.ts;
+        const [, isAdmin] = await Promise.all([
+            client.conversations
+                .join({
+                    channel: channel.id,
+                })
+                .catch(() => {}),
+            isUserAdmin(user.id),
+        ]);
+        const thread_ts = message.thread_ts || message.ts;
 
-    if (!thread_ts) {
-      await postEphemeral(channel.id, user.id, '❌ This is not a thread');
-      return;
-    }
-    if (!isAdmin) {
-      await postEphemeral(
-        channel.id,
-        user.id,
-        '❌ Only admins can run this command.',
-        thread_ts
-      );
-      return;
-    }
+        if (!thread_ts) {
+            await postEphemeral(channel.id, user.id, '❌ This is not a thread');
+            return;
+        }
+        if (!isAdmin) {
+            await postEphemeral(
+                channel.id,
+                user.id,
+                '❌ Only admins can run this command.',
+                thread_ts
+            );
+            return;
+        }
 
-    const thread = await prisma.thread.findFirst({
-      where: {
-        id: thread_ts,
-      },
+        const thread = await prisma.thread.findFirst({
+            where: {
+                id: thread_ts,
+            },
+        });
+
+        if (!thread || !thread.active) {
+            await client.views.open({
+                trigger_id: trigger_id,
+                view: {
+                    type: 'modal',
+                    callback_id: 'lock_modal',
+                    private_metadata: JSON.stringify({
+                        thread_id: thread_ts,
+                        channel_id: channel.id,
+                    }),
+                    title: {
+                        type: 'plain_text',
+                        text: 'Lock the thread',
+                        emoji: true,
+                    },
+                    submit: {
+                        type: 'plain_text',
+                        text: 'Submit',
+                        emoji: true,
+                    },
+                    close: {
+                        type: 'plain_text',
+                        text: 'Cancel',
+                        emoji: true,
+                    },
+                    blocks: [
+                        {
+                            type: 'section',
+                            text: {
+                                type: 'mrkdwn',
+                                text: 'Use this modal to lock the thread. Please note, this is logged.',
+                            },
+                        },
+                        {
+                            type: 'input',
+                            element: {
+                                type: 'plain_text_input',
+                                action_id: 'plain_text_input-action',
+                            },
+                            label: {
+                                type: 'plain_text',
+                                text: 'Reason',
+                                emoji: true,
+                            },
+                        },
+                        {
+                            type: 'input',
+                            element: {
+                                type: 'datetimepicker',
+                                action_id: 'datetimepicker-action',
+                            },
+                            label: {
+                                type: 'plain_text',
+                                text: 'Expires',
+                                emoji: true,
+                            },
+                        },
+                    ],
+                },
+            });
+            return;
+        } else {
+            await prisma.thread.update({
+                where: {
+                    id: thread_ts,
+                },
+                data: {
+                    active: false,
+                },
+            });
+
+            await Promise.all([
+                logInternal(
+                    `<@${user.id}> unlocked a thread in <#${channel.id}>.\nLink: ${getThreadLink(channel.id, thread_ts)}`
+                ),
+                removeReaction(channel.id, 'lock', thread_ts),
+            ]);
+        }
     });
 
-    if (!thread || !thread.active) {
-      await client.views.open({
-        trigger_id: trigger_id,
-        view: {
-          type: 'modal',
-          callback_id: 'lock_modal',
-          private_metadata: JSON.stringify({
-            thread_id: thread_ts,
-            channel_id: channel.id,
-          }),
-          title: {
-            type: 'plain_text',
-            text: 'Lock the thread',
-            emoji: true,
-          },
-          submit: {
-            type: 'plain_text',
-            text: 'Submit',
-            emoji: true,
-          },
-          close: {
-            type: 'plain_text',
-            text: 'Cancel',
-            emoji: true,
-          },
-          blocks: [
-            {
-              type: 'section',
-              text: {
-                type: 'mrkdwn',
-                text: 'Use this modal to lock the thread. Please note, this is logged.',
-              },
-            },
-            {
-              type: 'input',
-              element: {
-                type: 'plain_text_input',
-                action_id: 'plain_text_input-action',
-              },
-              label: {
-                type: 'plain_text',
-                text: 'Reason',
-                emoji: true,
-              },
-            },
-            {
-              type: 'input',
-              element: {
-                type: 'datetimepicker',
-                action_id: 'datetimepicker-action',
-              },
-              label: {
-                type: 'plain_text',
-                text: 'Expires',
-                emoji: true,
-              },
-            },
-          ],
-        },
-      });
-      return;
-    } else {
-      await prisma.thread.update({
-        where: {
-          id: thread_ts,
-        },
-        data: {
-          active: false,
-        },
-      });
+    app.shortcut('lock_thread_forever', async ({ ack, body, client }) => {
+        ack();
 
-      if (isUserAPIAvailable) {
-        try {
-          await unlockThread(channel.id, thread_ts);
-        } catch { }
-      }
+        if (body.type !== 'message_action') return;
+        const { channel, message, user } = body;
 
-      await Promise.all([
-        logInternal(
-          `<@${user.id}> unlocked a thread in <#${channel.id}>.\nLink: ${getThreadLink(channel.id, thread_ts)}`
-        ),
-        removeReaction(channel.id, 'lock', thread_ts),
-      ]);
-    }
-  });
+        const isAdmin = await isUserAdmin(user.id);
+        const thread_ts = message.thread_ts || message.ts;
 
-  app.shortcut('lock_thread_forever', async ({ ack, body, client }) => {
-    ack();
+        if (!thread_ts) {
+            await postEphemeral(channel.id, user.id, '❌ This is not a thread');
+            return;
+        }
+        if (!isAdmin) {
+            await postEphemeral(
+                channel.id,
+                user.id,
+                '❌ Only admins can run this command.',
+                thread_ts
+            );
+            return;
+        }
 
-    if (body.type !== 'message_action') return;
-    const { channel, message, user } = body;
+        await Promise.all([
+            prisma.log.create({
+                data: {
+                    thread_id: thread_ts,
+                    admin: user.id,
+                    lock_type: 'lock',
+                    time: new Date('9999-01-01T00:00:00.000Z'),
+                    reason: '(none)',
+                    channel: channel.id,
+                    active: true,
+                },
+            }),
+            prisma.thread.upsert({
+                where: {
+                    id: thread_ts,
+                },
+                create: {
+                    id: thread_ts,
+                    admin: user.id,
+                    lock_type: 'forever',
+                    time: new Date('9999-01-01T00:00:00.000Z'),
+                    reason: '(none)',
+                    channel: channel.id,
+                    active: true,
+                },
+                update: {
+                    admin: user.id,
+                    lock_type: 'forever',
+                    time: new Date('9999-01-01T00:00:00.000Z'),
+                    reason: '(none)',
+                    channel: channel.id,
+                    active: true,
+                },
+            }),
+        ]);
 
-    const isAdmin = await isUserAdmin(user.id);
-    const thread_ts = message.thread_ts || message.ts;
+        const logMessage = `<@${user.id}> locked a thread in <#${channel.id}> indefinitely.\nLink: ${getThreadLink(channel.id, thread_ts)}`;
 
-    if (!thread_ts) {
-      await postEphemeral(channel.id, user.id, '❌ This is not a thread');
-      return;
-    }
-    if (!isAdmin) {
-      await postEphemeral(
-        channel.id,
-        user.id,
-        '❌ Only admins can run this command.',
-        thread_ts
-      );
-      return;
-    }
+        const [publicLogResult] = await Promise.all([
+            client.chat.postMessage({
+                channel: env.SLACK_LOG_CHANNEL,
+                text: logMessage,
+            }),
+            logInternal(logMessage),
+            postMessage(channel.id, `This thread is locked indefinitely.`, thread_ts),
+            removeReaction(channel.id, 'lock', thread_ts),
+        ]);
 
-    await Promise.all([
-      prisma.log.create({
-        data: {
-          thread_id: thread_ts,
-          admin: user.id,
-          lock_type: 'lock',
-          time: new Date('9999-01-01T00:00:00.000Z'),
-          reason: '(none)',
-          channel: channel.id,
-          active: true,
-        },
-      }),
-      prisma.thread.upsert({
-        where: {
-          id: thread_ts,
-        },
-        create: {
-          id: thread_ts,
-          admin: user.id,
-          lock_type: 'forever',
-          time: new Date('9999-01-01T00:00:00.000Z'),
-          reason: '(none)',
-          channel: channel.id,
-          active: true,
-        },
-        update: {
-          admin: user.id,
-          lock_type: 'forever',
-          time: new Date('9999-01-01T00:00:00.000Z'),
-          reason: '(none)',
-          channel: channel.id,
-          active: true,
-        },
-      }),
-    ]);
-
-    const logMessage = `<@${user.id}> locked a thread in <#${channel.id}> indefinitely.\nLink: ${getThreadLink(channel.id, thread_ts)}`;
-
-    const [publicLogResult] = await Promise.all([
-      client.chat.postMessage({
-        channel: env.SLACK_LOG_CHANNEL,
-        text: logMessage,
-      }),
-      logInternal(logMessage),
-      postMessage(channel.id, `This thread is locked indefinitely.`, thread_ts),
-      removeReaction(channel.id, 'lock', thread_ts),
-    ]);
-
-    if (isUserAPIAvailable) {
-      try {
-        await lockThread(channel.id, thread_ts);
-      } catch { }
-
-      if (publicLogResult?.ts) {
-        try {
-          await lockMessage(env.SLACK_LOG_CHANNEL, publicLogResult.ts);
-        } catch { }
-      }
-    }
-  });
+        if (isUserAPIAvailable && publicLogResult?.ts) {
+            try {
+                await lockMessage(env.SLACK_LOG_CHANNEL, publicLogResult.ts);
+            } catch {}
+        }
+    });
 }
 
 export default registerShortcuts;
